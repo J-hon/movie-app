5505976a72c070dd69900f4ccf49651a1491da94
J-hon <Jonadify007@gmail.com>
2022-10-19 15:04:49 +0100

    Adds one more test & Refactors MoviesTest

--- tests/Feature/MoviesTest.php
+++ tests/Feature/MoviesTest.php
@@ -9,51 +9,60 @@ use Tests\TestCase;
 class MoviesTest extends TestCase
 {
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->user  = $this->authenticate(User::factory()->create());
+    }
+
     public function test_user_can_add_movie_to_list()
     {
-        $user  = $this->authenticate(User::factory()->create());
         $movie = Movie::factory()->create();
 
         $this->postJson("$this->baseUrl/movies/list/add", ['movie_id' => $movie->id])->assertStatus(200);
 
         $this->assertDatabaseHas('movie_lists', [
-            'user_id'  => $user->id,
+            'user_id'  => $this->user->id,
             'movie_id' => $movie->id
         ]);
     }
 
     public function test_user_cant_add_same_movie_to_list()
     {
-        $user  = $this->authenticate(User::factory()->create());
         $movie = Movie::factory()->create();
 
-        $user->movies()->attach($movie->id);
+        $this->user->movies()->attach($movie->id);
 
         $this->postJson("$this->baseUrl/movies/list/add", ['movie_id' => $movie->id])->assertStatus(400);
     }
 
     public function test_user_cant_add_more_than_ten_movies_to_list()
     {
-        $user  = $this->authenticate(User::factory()->create());
         $movie = Movie::factory()->create();
 
-        $user->movies()->attach(Movie::factory(10)->create()->pluck('id')->toArray());
+        $this->user->movies()->attach(Movie::factory(10)->create()->pluck('id')->toArray());
 
         $this->postJson("$this->baseUrl/movies/list/add", ['movie_id' => $movie->id])->assertStatus(400);
     }
 
     public function test_user_can_remove_movie_from_list()
     {
-        $user  = $this->authenticate(User::factory()->create());
         $movie = Movie::factory()->create();
 
-        $user->movies()->attach($movie->id);
+        $this->user->movies()->attach($movie->id);
 
         $this->deleteJson("$this->baseUrl/movies/list/remove", ['movie_id' => $movie->id])->assertStatus(200);
 
         $this->assertDatabaseMissing('movie_lists', [
-            'user_id'  => $user->id,
+            'user_id'  => $this->user->id,
             'movie_id' => $movie->id
         ]);
+    }
+
+    public function test_user_cant_remove_non_existing_movie_from_list()
+    {
+        $movie = Movie::factory()->create();
+
+        $this->deleteJson("$this->baseUrl/movies/list/remove", ['movie_id' => $movie->id])->assertStatus(400);
     }
 }
